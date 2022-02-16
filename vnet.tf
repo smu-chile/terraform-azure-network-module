a7b1b8bf3efd4bd9eec60748abf76d2f4eac4a3e
Cristobal Diaz <cdiazm@smu.cl>
2022-02-16 08:20:11 -0300

    module netwrok 8

--- vnet.tf
+++ vnet.tf
@@ -16,9 +16,8 @@ resource "azurerm_virtual_network" "vnet-a" {
         }
 }
 resource "azurerm_subnet" "subnet-a-0" {
-  count                     = 2
-  name                      = (cl-a0-${var.name}-${var.environment}-${var.regions}-, count.index + 1)
+  count                     = 1
+  name                      = "cl-a0-${var.name}-${var.environment}-${var.regions}"
   resource_group_name       = var.resource_group_name
   virtual_network_name      = azurerm_virtual_network.vnet-a.name
-  address_prefixes          = ([${element(split(",", var.address_network), count.index)}]
-}
+  address_prefixes          = element(split(",", var.address_spaces), count.index)
